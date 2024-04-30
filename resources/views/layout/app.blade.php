c33cb6022b3ec1325029746d61d80cb2aefe4e47
Nay Ba La <naybala.zotefamily@gmail.com>
2024-04-30 17:56:32 +0630

    add index page

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -13,6 +13,18 @@
 </head>
 
 <body class="overflow-x-hidden">
+    <div>
+        @if (url()->current() == 'http://127.0.0.1:8000')
+            <a href="{{ route('books.index') }}" class="">
+                <button type="button"
+                    class="text-white bg-gray-800 hover:bg-gray-900 focus:outline-none focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-700 dark:border-gray-700">
+                    GO
+                </button>
+            </a>
+        @endif
+
+    </div>
+
     @yield('content')
 </body>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/flowbite.min.js"></script>
--- routes/web.php
+++ routes/web.php
@@ -3,4 +3,7 @@
 use App\Http\Controllers\BookController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('layout.app');
+});
 Route::resource('/books', BookController::class);
